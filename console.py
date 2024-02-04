8a6bfe0b270ad5a9f828def9b8c2da49e0d3887b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-04 12:14:28 -0800

    Adding basic methods for number 7, not complete yet

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 Opens a command line interpreter and prompts user for a command
 """
 import cmd
+import sys
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +26,25 @@ class HBNBCommand(cmd.Cmd):
         """Empty line"""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        boop
+
+    def do_show(self, arg):
+        """Prints an instance"""
+        boop
+
+    def do_destroy(self, arg):
+        """Deletes an instance"""
+        boop
+
+    def do_all(self, arg):
+         """Prints all instances"""
+         boop
+
+    def do_update(self, arg):
+        """Updates an instance"""
+        boop
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
