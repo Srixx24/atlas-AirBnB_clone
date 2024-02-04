1db569ca20e088367c9b0e7229d0cb9ab5bf3698
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-03 16:54:57 -0800

    Updating file storage engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,25 +4,50 @@
 The engine that runs file storage, 
 with various methods for JSON serialization
 """
+import json
+
 
 class FileStorage:
+    """
+    Start of FileStorage instance 
+    """
+    # Set filepath to file.json for data storage
+    __file_path = 'file.json'
+    # Empty dictionary to hold object IDs and objects
+    # formatted as such: <classname>.<IDnumber>
+    __objects = {}
 
-    #Set filepath to file.json for data storage
-    __file_path: 'file.json'
-    #Empty dictionary to hold object IDs and objects
-    #formatted as such: <classname>.<IDnumber>
-    __objects: {}
-
-    #returns a dictionary of objects
+    # Returns a dictionary of objects
     def all(self):
-        return(self.__objects)
+        """Returns dict objects"""
+        return self.__objects
 
-    #saves new object into the dictionary
-    #key - [<classname>.id#]
+    # Saves new object into the dictionary
+    # Key - [<classname>.id#]
     def new(self, obj):
-        self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
+        """Add new obj to dict"""
+        key = f'{obj.__class__.__name__}.{obj.id}'
+        self.__objects[key] = obj
 
     def save(self):
+        """Saves to existing instances"""
+        with open(self.__class__.__file_path, "w") as file:
+            # Serialize objects 
+            objects = {key: val.to_dict() for
+                    key, val in self.__objects.items()}
+            # Write serialized obj to file
+            json.dump(objects, file)
         
 
     def reload(self):
+        """Load intance from save"""
+        with open(self.__file_path, "r") as file:
+            # Loads obj from file
+            objects = json.load(file)
+            for key in obj.keys():
+                # Extract class name
+                class_name = value["__class__"]
+                # Creates new instance of the class
+                new_obj = globals()[class_name](**value)
+                # Adds new obj to the dict
+                self.__objects[key] = new_obj
