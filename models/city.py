10dca708a27ce7abcdd0d2ef3c4f9b09b380f313
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2024-02-05 12:11:31 -0800

    Adding remaining classes

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class City that inherit from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Start of class City instance
+    """
+    state_id = ""
+    name = ""
